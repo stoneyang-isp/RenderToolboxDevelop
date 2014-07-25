59d54ebdcd936185aa0bece130128d7f3812195e
Ben Heasly <benjamin.heasly@gmail.com>
2014-07-25 19:34:25 -0400

    Can now insert multiple objects simultaneously and detect them separately.

--- wildScenes/MakeObjectMask/MakeObjectMask.m
+++ wildScenes/MakeObjectMask/MakeObjectMask.m
@@ -8,6 +8,7 @@ clear;
 %% Locate files of interest.
 wildPath = fileparts(mfilename('fullpath'));
 parentScene = fullfile(wildPath, 'tame/scenes/IndoorPlant.dae');
+
 objectPath = fullfile(wildPath, 'tame/objects');
 objects = { ...
     fullfile(objectPath, 'Barrel.dae'), ...
@@ -21,23 +22,53 @@ mappingsFile = 'ObjectMaskMappings.txt';
 % WriteDefaultMappingsFile( ...
 %     parentSceneFile, mappingsFile, '', white, white);
 
-% conditions file generated from objects
-wls = 300:10:800;
-reflectance1 = GetSingleBandReflectance(wls, 27, 1);
-reflectances1 = repmat({reflectance1}, numel(objects), 1);
-varNames = {'object1', 'reflectance1'};
-varValues = cat(2, objects', reflectances1);
+nConditions = 4;
+nObjects = 4;
+
+allNames = {};
+allValues = {};
+for ii = 1:nObjects
+    % choose object reflectance
+    wls = 300:10:800;
+    reflectance = GetSingleBandReflectance(wls, 10+ii, 1);
+    reflectances = repmat({reflectance}, 1, nConditions);
+    
+    % choose the position for each object
+    %   bound by a box that I determined by poking around in Blender
+    xRange = [-6 2];
+    yRange = [-2 2];
+    zRange = [0 6];
+    position = cell(1, nConditions);
+    for cc = 1:nConditions
+        position{cc} = GetRandomPosition(xRange, yRange, zRange);
+    end
+    
+    % choose a random object for each condition
+    object = objects(randi(numel(objects), [1, nConditions]));
+    
+    % pack up variable names and values
+    objectName = sprintf('object-%d', ii);
+    reflectanceName = sprintf('reflectance-%d', ii);
+    positionName = sprintf('position-%d', ii);
+    varNames = {objectName, reflectanceName, positionName};
+    varValues = cat(2, object', reflectances', position');
+    
+    % append for conditions file creation
+    allNames = cat(2, allNames, varNames);
+    allValues = cat(2, allValues, varValues);
+end
+
 conditionsFile = 'ObjectMaskConditions.txt';
-WriteConditionsFile(conditionsFile, varNames, varValues);
+WriteConditionsFile(conditionsFile, allNames, allValues);
 
 %% Choose batch renderer options.
-hints.imageWidth = 320;
-hints.imageHeight = 240;
+hints.imageWidth = 640;
+hints.imageHeight = 480;
 hints.recipeName = 'MakeWildScene';
 hints.renderer = 'Mitsuba';
 hints.remodeler = 'InsertObjectRemodeler';
 
-hints.whichConditions = 1;
+hints.whichConditions = 1:nConditions;
 
 %% Render all objects!
 nativeSceneFiles = MakeSceneFiles(parentScene, conditionsFile, mappingsFile, hints);
@@ -45,37 +76,46 @@ radianceDataFiles = BatchRender(nativeSceneFiles, hints);
 
 %% Try to find the objects.
 close all
-clc
 
 toneMapFactor = 100;
 isScale = true;
 
 pixelThreshold = 0.1;
-pixelMaskRgb = [0 255 0];
+pixelMaskRgb = 255*[ ...
+    0 0 1; ...
+    0 1 0; ...
+    0 1 1; ...
+    1 0 0; ...
+    1 0 1; ...
+    1 1 0; ...
+    1 1 1];
 
 imageFolder = GetWorkingFolder('images', true, hints);
-for oo = 1:numel(hints.whichConditions)
-    rendering = load(radianceDataFiles{oo});
+for cc = 1:numel(hints.whichConditions)
+    rendering = load(radianceDataFiles{cc});
     imageSize = size(rendering.multispectralImage);
     
     % save the sRgb image
-    objectFile = objects{hints.whichConditions(oo)};
-    [objectPath, objectName] = fileparts(objectFile);
+    objectName = sprintf('%d-MakeObjectMask', cc);
     imageFile = fullfile(imageFolder, [objectName '-srgb.png']);
-    sRgbImage = MakeMontage(radianceDataFiles(oo), imageFile, toneMapFactor, isScale, hints);
+    sRgbImage = MakeMontage(radianceDataFiles(cc), imageFile, toneMapFactor, isScale, hints);
     sRGBMasked = uint8(sRgbImage);
     
     wls = MakeItWls(rendering.S);
-    
+    %figure();
+
     for ii = 1:imageSize(1)
         for jj = 1:imageSize(2)
             pixelSpectrum = squeeze(rendering.multispectralImage(ii,jj,:));
             isHigh = pixelSpectrum > max(pixelSpectrum)*pixelThreshold;
             if sum(isHigh) == 1
-                sRGBMasked(ii,jj,:) = pixelMaskRgb;
+                whichObject = find(isHigh);
+                sRGBMasked(ii,jj,:) = pixelMaskRgb(whichObject,:);
+                %line(wls, pixelSpectrum, 'Marker', 'none', 'LineStyle', '-')
             end
         end
     end
+    %set(gca, 'XTickLabel', wls)
     
     % save the mask image
     maskFile = fullfile(imageFolder, [objectName '-mask.png']);
