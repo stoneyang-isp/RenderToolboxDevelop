b2c07bb6792d07db505a7496708ca53b3516bf83
Ben Heasly <benjamin.heasly@gmail.com>
2015-04-03 11:04:30 -0400

    TestTwoPointCorrelationDistribution looks at scrambled images.

--- VirtualScenesToolbox/Examples/WardLandFourier/TestTwoPointCorrelationDistribution.m
+++ VirtualScenesToolbox/Examples/WardLandFourier/TestTwoPointCorrelationDistribution.m
@@ -3,44 +3,101 @@
 clear
 clc
 
+%% Get some images.
 naturalImages = '/Users/ben/Documents/Projects/UPennNaturalImages/tofu.psych.upenn.edu/zip_nxlhtvdlbb/cd16A';
 
-imageFileA = fullfile(naturalImages, 'DSC_0001_LUM.mat');
-imageDataA = load(imageFileA);
-imageA = imageDataA.LUM_Image;
+imageFileRoad = fullfile(naturalImages, 'DSC_0001_LUM.mat');
+imageDataRoad = load(imageFileRoad);
+imageRoad = imageDataRoad.LUM_Image;
 
-imageFileB = fullfile(naturalImages, 'DSC_0003_LUM.mat');
-imageDataB = load(imageFileB);
-imageB = imageDataB.LUM_Image;
+imageFileFence = fullfile(naturalImages, 'DSC_0003_LUM.mat');
+imageDataFence = load(imageFileFence);
+imageFence = imageDataFence.LUM_Image;
 
+nElements = numel(imageRoad);
+imageRoadScramble = zeros(size(imageRoad));
+imageRoadScramble(1:nElements) = imageRoad(randperm(nElements));
+
+imageFenceScramble = zeros(size(imageFence));
+imageFenceScramble(1:nElements) = imageFence(randperm(nElements));
+
+%% Choose some analysis and plot params.
 binEdges = 0:5:200;
 samplesPerBin = 5000;
 
-subplot(3, 2, 1)
-imshow(imageA, [0, max(imageA(:))])
+nRows = 5;
 
-subplot(3, 2, 2)
+set(gcf(), 'Position', [100 100 1000 1000]);
+fontSize = 16;
+
+%% Road vs Road.
+subplot(nRows, 2, 1)
+imshow(imageRoad, [0, max(imageRoad(:))])
+title('road');
+set(gca(), 'FontSize', fontSize);
+
+subplot(nRows, 2, 2)
 [inCorrelations, outCorrelations, nIn, binEdges] = ...
-    TwoPointCorrelationDistribution(imageA, imageA, binEdges, samplesPerBin);
+    TwoPointCorrelationDistribution(imageRoad, imageRoad, binEdges, samplesPerBin);
 binTops = binEdges(2:end);
 plot(binTops, inCorrelations, binTops, outCorrelations);
-ylim([0, 1])
+ylim([-0.2, 1])
+title('road vs road');
+set(gca(), 'YGrid', 'on', 'FontSize', fontSize);
 
-subplot(3, 2, 3)
-imshow(imageB, [0, max(imageB(:))])
+%% Fence vs Fence.
+subplot(nRows, 2, 3)
+imshow(imageFence, [0, max(imageFence(:))])
+title('fence')
+set(gca(), 'FontSize', fontSize);
 
-subplot(3, 2, 4)
+subplot(nRows, 2, 4)
 [inCorrelations, outCorrelations, nIn, binEdges] = ...
-    TwoPointCorrelationDistribution(imageB, imageB, binEdges, samplesPerBin);
+    TwoPointCorrelationDistribution(imageFence, imageFence, binEdges, samplesPerBin);
 binTops = binEdges(2:end);
 plot(binTops, inCorrelations, binTops, outCorrelations);
-ylim([0, 1])
+ylim([-0.2, 1])
+title('fence vs fence');
+set(gca(), 'YGrid', 'on', 'FontSize', fontSize);
 
-subplot(3, 2, 6)
+%% Road vs Fence.
+subplot(nRows, 2, 6)
 [inCorrelations, outCorrelations, nIn, binEdges] = ...
-    TwoPointCorrelationDistribution(imageA, imageB, binEdges, samplesPerBin);
+    TwoPointCorrelationDistribution(imageRoad, imageFence, binEdges, samplesPerBin);
 binTops = binEdges(2:end);
 plot(binTops, inCorrelations, binTops, outCorrelations);
-ylim([0, 1])
+ylim([-0.2, 1])
+title('road vs fence');
+set(gca(), 'YGrid', 'on', 'FontSize', fontSize);
 
-set(gcf(), 'Position', [100 100 1000 1000])
+%% Road vs Road Scramble.
+subplot(nRows, 2, 7)
+imshow(imageRoadScramble, [0, max(imageRoadScramble(:))])
+title('road scramble')
+set(gca(), 'FontSize', fontSize);
+
+subplot(nRows, 2, 8)
+[inCorrelations, outCorrelations, nIn, binEdges] = ...
+    TwoPointCorrelationDistribution(imageRoad, imageRoadScramble, binEdges, samplesPerBin);
+binTops = binEdges(2:end);
+plot(binTops, inCorrelations, binTops, outCorrelations);
+ylim([-0.2, 1])
+title('road vs road scramble');
+set(gca(), 'YGrid', 'on');
+set(gca(), 'YGrid', 'on', 'FontSize', fontSize);
+
+%% Fence Scramble vs Road Scramble.
+subplot(nRows, 2, 9)
+imshow(imageFenceScramble, [0, max(imageFenceScramble(:))])
+title('fence scramble')
+set(gca(), 'FontSize', fontSize);
+
+subplot(nRows, 2, 10)
+[inCorrelations, outCorrelations, nIn, binEdges] = ...
+    TwoPointCorrelationDistribution(imageRoad, imageRoadScramble, binEdges, samplesPerBin);
+binTops = binEdges(2:end);
+plot(binTops, inCorrelations, binTops, outCorrelations);
+ylim([-0.2, 1])
+title('fence scramble vs road scramble');
+set(gca(), 'YGrid', 'on');
+set(gca(), 'YGrid', 'on', 'FontSize', fontSize);
