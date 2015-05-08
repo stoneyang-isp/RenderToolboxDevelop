3025e60153e54f0ccc01697fa0e9efc821632a6b
Ben Heasly <benjamin.heasly@gmail.com>
2015-05-08 17:27:06 -0400

    Starting standalone Matlab executable for rendering recipes.

---
+++ VirtualScenesToolbox/Examples/Standalone/RecipeExecutor.m
@@ -0,0 +1,75 @@
+%% Render Recipes as a standalone Matlab executable.
+%   @param recipeFile packed-up recipe archive from PackUpRecipe()
+%   @param whichExecutives passed to ExecuteRecipe()
+%   @param beforeScript optional script to run before executing recipe
+%   @param afterScript optional script to run after executing recipe
+%
+% @details
+% Expects 1-3 parameters from the command line.  These should all be file
+% names that can be found from the current directly.  If @a beforeScript is
+% found, it's executed first.  Then the given @a recipeFile is unarchived
+% and executed.  Then, if @a afterScript is found, it's executed.  Will
+% attempt to execute @a afterScript even if there's an error during recipe
+% execution.
+%
+% @details
+% Something we would like to solve: recipes invoke "random" functions
+% specified in their "executive" cell array via feval().  For this to work,
+% we need to tell MCC about them explicitly ("%#function" pragma or command
+% args).  We can add a bunch of "best guess" pragmas here.  Can we find a
+% way to incorporate "random" user functions without having to build a
+% separate standalone for each recipe?
+%
+% @details
+% Usage as a regular function in Matlab:
+%   function RecipeExecutor(recipeFile, whichExecutives, beforeScript, afterScript)
+%
+% Usage as a standalone on the command line:
+%   ./run_RecipeExecutor.sh/Applications/MATLAB/MATLAB_Compiler_Runtime/v84 recipeFile [whichExecutives] [beforeScript] [afterScript]
+%
+function RecipeExecutor(recipeFile, whichExecutives, beforeScript, afterScript)
+
+% tell Matlab about functions that recipes will need
+%#function MakeRecipeRenderings, MakeRecipeSceneFiles
+
+% TODO: tell Matlab about Renderer and Remodeler API functions!
+
+if nargin < 1 || isempty(recipeFile)
+    recipeFile = '';
+end
+
+if nargin < 2 || isempty(whichExecutives)
+    whichExecutives = [];
+end
+if ischar(whichExecutives)
+    whichExecutives = eval(whichExecutives);
+end
+
+if nargin < 3 || isempty(beforeScript)
+    beforeScript = '';
+end
+
+if nargin < 4 || isempty(afterScript)
+    afterScript = '';
+end
+
+if ~exist(recipeFile, 'file')
+    error('RecipeExecutor:NoSuchRecipe', ...
+        'recipeFile not found: %s', recipeFile);
+end
+
+% Get the Recipe.
+recipe = UnpackRecipe(recipeFile);
+
+% Set Up.
+if exist(beforeScript, 'file')
+    run(beforeScript);
+end
+
+% Execute the Recipe.
+recipe = ExecuteRecipe(recipe, whichExecutives, false);
+
+% Tear Down.
+if exist(afterScript, 'file')
+    run(afterScript);
+end
